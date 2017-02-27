50f31d38de147b761562ede135de29e7b862acb6
PierrickHERVE <pierrick.herve56@gmail.com>
2017-02-27 11:02:17 +0100

    grille de jeu et reserve de billes à placer

--- www/jeu.php
+++ www/jeu.php
@@ -15,6 +15,7 @@
 
     <body>
     
+
 		<div id="mettre_au_milieu_de_la_page">
 			<div id="grille_des_niveaux">
 				
@@ -27,7 +28,77 @@
 					
 					</div>
 					<div id="grille_de_jeu">
-					
+					<div>
+
+					</div>
+						<table>
+							<tr>
+								<td>a</td>
+								<td>a</td>
+								<td>a</td>
+								<td>a</td>
+							</tr>
+							<tr>
+								<td>q</td>
+								<td>s</td>
+								<td>d</td>
+								<td>f</td>
+							</tr>
+							<tr>
+								<td>h</td>
+								<td>u</td>
+								<td>d</td>
+								<td>j</td>
+							</tr>
+							<tr>
+								<td>q</td>
+								<td>s</td>
+								<td>d</td>
+								<td>f</td>
+							</tr>
+							<tr>
+								<td>h</td>
+								<td>u</td>
+								<td>d</td>
+								<td>j</td>
+							</tr>
+							<tr>
+								<td>q</td>
+								<td>s</td>
+								<td>d</td>
+								<td>f</td>
+							</tr>
+							<tr>
+								<td>h</td>
+								<td>u</td>
+								<td>d</td>
+								<td>j</td>
+							</tr>
+							<tr>
+								<td>q</td>
+								<td>s</td>
+								<td>d</td>
+								<td>f</td>
+							</tr>
+							<tr>
+								<td>h</td>
+								<td>u</td>
+								<td>d</td>
+								<td>j</td>
+							</tr>
+							<tr>
+								<td>q</td>
+								<td>s</td>
+								<td>d</td>
+								<td>f</td>
+							</tr>
+							<tr>
+								<td>h</td>
+								<td>u</td>
+								<td>d</td>
+								<td>j</td>
+							</tr>
+						</table>
 					</div>
 
 				</div>
@@ -41,13 +112,89 @@
 						
 					</div>
 					<div id="bille_à_placer">
+						<a href="#", class="button" id="bille1"> Bouton 1</a>
+						<p>blaazdzadz</p>
 
+						<table>
+							<tr>
+								<td id="bille1" >a</td>
+								<td id="bille2" >a</td>
+								<td id="bille3" >a</td>
+							</tr>
+							<tr>
+								<td id="bille4" >a</td>
+								<td id="bille5" >a</td>
+								<td id="bille6" >a</td>
+							</tr>
+							<tr>
+								<td id="bille7" >a</td>
+								<td id="bille8" >a</td>
+								<td id="bille9" >a</td>
+							</tr>
+							<tr>
+								<td id="bille10" >a</td>
+								<td id="bille11" >a</td>
+								<td id="bille12" >a</td>
+							</tr>
+							<tr>
+								<td id="bille13" >a</td>
+								<td id="bille14" >a</td>
+								<td id="bille15" >a</td>
+							</tr>
+							</table>
 					</div>
 				</div>
 
 			</div>
 
 		</div>
+
+
+<p>This example uses the HTML DOM to assign an "onclick" event to a p element.</p>
+
+<p id="bille1">Click me.</p>
+
+
+
+
+
+
+<script>
+/*	var bille1 = document.getElementById("bille1");*/
+	var bille2 = document.getElementById("bille2");
+	var bille3 = document.getElementById("bille3");
+	var bille4 = document.getElementById("bille4");
+	var bille5 = document.getElementById("bille5");
+	var bille6 = document.getElementById("bille6");
+	var bille7 = document.getElementById("bille7");
+	var bille8 = document.getElementById("bille8");
+	var bille9 = document.getElementById("bille9");
+	var bille10 = document.getElementById("bille10");
+	var bille11 = document.getElementById("bille11");
+	var bille12 = document.getElementById("bille12");
+	var bille13 = document.getElementById("bille13");
+	var bille14 = document.getElementById("bille14");
+	var bille15 = document.getElementById("bille15");
+
+
+
+/*	bille1.onClick = function() {
+		on passe de snake_case dans le CSS a camelCase dans le JavaScript
+		r.style.backgroundColor = 'black';
+		write("bla");
+		/*r.style.visibility = 'hiden';
+	}*/
+
+
+
+document.getElementById("bille1").onclick = function() {myFunction()};
+
+function myFunction() {
+    r.style.backgroundColor = 'black';
+}
+
+</script>
+
 		
     </body>
 
