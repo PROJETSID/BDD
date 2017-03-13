a6844da2c19cb354a53af005beec87895bfd1717
PierrickHERVE <pierrick.herve56@gmail.com>
2017-03-13 11:34:15 +0100

    réorganisation du dépôt

--- bla/head.php
+++
@@ -1,9 +0,0 @@
-<!-- menu commun à toutes les pages -->
-<div>
-	<nav id="mettre_au_milieu_de_la_page">
-		<a class="menu_choix_1" href="index.php">Acceuil</a>
- 		<a class="menu_choix_1" href="authentification.php">Authentification</a>
-		<a class="menu_choix_2" href="jeu.php">Jeu</a>
-		<a class="menu_choix_3" href="highScores.php">High Scores</a>
-	</nav>
-<div>
