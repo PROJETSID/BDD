166c0a4392e53dc2ce220bfb5d94b70ec9de543e
Serigne <diawahad@gmail.com>
2017-04-07 16:44:37 +0200

    partie highsore sans les selections coté BD

---
+++ WEB/www/highscoretous.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8" />
+
+        <title>High Scores</title>
+        <link rel="stylesheet" href="style.css" />
+        <?php
+       		include("head.php");
+		include("db/connect.php");
+        ?>
+    </head>
+	<style>
+	#tableau3
+	{
+		flex: 5;
+		margin-top: 20px;
+		background-color: grey
+	}
+	</style>
+    <body>
+
+        <div id="mettre_au_milieu_de_la_page">
+            <div id="high_scores">
+                <!-- menu choix : jour/semaine/tous -->
+                <nav id="menu_choix_hs">
+                    <a href="http://localhost/mastermind/www/highscoreJour.php" class="button" id="hs_jour">Jour</a>
+                    <a href="http://localhost/mastermind/www/highscoresemaine.php" class="button" id="hs_semaine">Semaine</a>
+                    <a href="http://localhost/mastermind/www/highscoretous.php" class="button" id="hs_tous">Tous</a>
+                </nav>
+                <!-- tableaux qui affichent les meilleurs joueurs (plus haut score) -->
+                <div id="tableau3">
+			   <p> tous les high scores</p>
+                </div>
+            </div>
+        </div>
+		
+    </body>
+
+</html>
+
